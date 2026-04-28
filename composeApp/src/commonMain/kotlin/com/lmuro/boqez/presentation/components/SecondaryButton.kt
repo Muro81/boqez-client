bdf2cc061ea7bedca9f345594b61429bbba12e60
Luka Murisic <murisicluka@gmail.com>
2026-04-28 10:56:51 +0200

    Updates home screen UI

---
+++ composeApp/src/commonMain/kotlin/com/lmuro/boqez/presentation/components/SecondaryButton.kt
@@ -0,0 +1,61 @@
+package com.lmuro.boqez.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.dropShadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.shadow.Shadow
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
+import com.lmuro.boqez.theme.BoqezThemeProvider
+
+@Composable
+fun SecondaryButton(
+    onClick: () -> Unit,
+    containerColor: Color = BoqezThemeProvider.colors.feltDark,
+    contentColor: Color = BoqezThemeProvider.colors.goldLight,
+    shape: Shape = RoundedCornerShape(8.dp),
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean = true,
+    buttonContent: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults
+            .buttonColors(
+                containerColor = if (isEnabled) containerColor else BoqezThemeProvider.colors.skyBase,
+                contentColor = if (isEnabled) contentColor else BoqezThemeProvider.colors.inkLighter
+            ),
+        shape = shape,
+        enabled = isEnabled,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .dropShadow(
+                shape = shape,
+                shadow = Shadow(
+                    color = BoqezThemeProvider.colors.crimsonBase.copy(alpha = 0.4f),
+                    radius = 18.dp,
+                    spread = 0.dp,
+                    offset = DpOffset(x = 0.dp, y = 4.dp)
+                )
+            )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            buttonContent()
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/com/lmuro/boqez/presentation/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/lmuro/boqez/presentation/home/HomeScreen.kt
@@ -1,13 +1,18 @@
 package com.lmuro.boqez.presentation.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -15,11 +20,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import boqez.composeapp.generated.resources.Res
+import boqez.composeapp.generated.resources.app_name
 import com.lmuro.boqez.core.utils.ObserveWithLifecycle
 import com.lmuro.boqez.presentation.base.BaseContentView
 import com.lmuro.boqez.presentation.components.PrimaryButton
+import com.lmuro.boqez.presentation.components.SecondaryButton
+import com.lmuro.boqez.presentation.components.TitleBlock
 import com.lmuro.boqez.presentation.home.components.LobbyPickerDialog
 import com.lmuro.boqez.theme.BoqezThemeProvider
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -52,56 +62,69 @@ fun HomeScreen(
                 viewModel.onEvent(HomeEvent.OnDissmissDialog)
             }
         )
-        IconButton(
-            onClick = {
-                viewModel.onEvent(HomeEvent.OnProfileClick)
-            },
-            modifier = Modifier
-                .align(Alignment.TopEnd)
-        ) {
-            Icon(
-                imageVector = Icons.Default.AccountCircle,
-                contentDescription = null,
-                tint = BoqezThemeProvider.colors.primaryDarkest
-            )
-        }
         Column(
             modifier = Modifier
+                .background(BoqezThemeProvider.colors.parchment)
                 .padding(horizontal = 30.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(20.dp)
         ) {
-            Spacer(Modifier.weight(1f))
+
+            TitleBlock(
+                modifier = Modifier.weight(1f),
+                appName = stringResource(Res.string.app_name)
+            )
+
             PrimaryButton(
                 onClick = {
                     viewModel.onEvent(HomeEvent.OnPlayClick)
                 }
             ) {
                 Text(
-                    text = "PLAY"
+                    text = "PLAY",
+                    style = BoqezThemeProvider.typography.cinzelBold16
                 )
             }
 
-            PrimaryButton(
+            SecondaryButton(
                 onClick = {
                     viewModel.onEvent(HomeEvent.OnSettingsClick)
                 }
             ) {
                 Text(
-                    text = "SETTINGS"
+                    text = "SETTINGS",
+                    style = BoqezThemeProvider.typography.cinzelBold16
                 )
             }
 
-            PrimaryButton(
-                onClick = {
-                    viewModel.onEvent(HomeEvent.OnAboutUsClick)
-                }
-            ) {
-                Text(
-                    text = "ABOUT US"
-                )
-            }
+//            PrimaryButton(
+//                onClick = {
+//                    viewModel.onEvent(HomeEvent.OnAboutUsClick)
+//                }
+//            ) {
+//                Text(
+//                    text = "ABOUT US"
+//                )
+//            }
             Spacer(Modifier.weight(1f))
+        }
+        IconButton(
+            onClick = {
+                viewModel.onEvent(HomeEvent.OnProfileClick)
+            },
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .offset((-10).dp,10.dp),
+            colors = IconButtonDefaults.iconButtonColors(
+                containerColor = BoqezThemeProvider.colors.feltDark,
+                contentColor = BoqezThemeProvider.colors.goldLight
+            )
+        ) {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = null,
+                tint = BoqezThemeProvider.colors.goldLight
+            )
         }
     }
 }
